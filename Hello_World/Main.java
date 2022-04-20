c0f295dcdd0cfcef3ae066a8f2f66d8a2cee8462
Alex Sutter <46454849+sutter12@users.noreply.github.com>
2022-04-20 11:48:05 -0400

    4-20-2022 Update
    HelloWorld added

---
+++ Hello_World/Main.java
@@ -0,0 +1,14 @@
+// Author: Alexander Sutter
+// Date: 4/20/2022
+// Latest Revision: 4/20/2022
+// Hello World: Main.java
+
+//Run
+// javac Main.java
+// java Main
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }//end main method
+}//end class Main
